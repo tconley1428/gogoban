889818323282c406ad2a8c662fbb50e1fa127cab
Timothy Conley <tconley1428@gmail.com>
2014-08-10 20:04:56 -0400

    Beginnings of a lobby system and alternating stone placements.

--- js/controllers.js
+++ js/controllers.js
@@ -3,15 +3,55 @@ var BOARD_SIZE = 19
 var GoGoBan = angular.module('GoGoBan', []);
 
 GoGoBan.controller('GoGoBanCtrl', function ($scope) {
+
+  var connection = new WebSocket('ws://'+window.location.host+'/ws/game', []);
+
+  connection.onmessage = function(e){
+  	obj = JSON.parse(e.data)
+  	$scope.board[obj.X][obj.Y] = {"Player":obj.Player}
+  	$scope.$apply()
+  }
+
   $scope.board = [];
   for (var i = 0; i < BOARD_SIZE; i++) {
 	  var row = [];
 	  for (var j = 0; j < BOARD_SIZE; j++) {
-	  	row.push("+");
+	  	row.push({"Player":"empty"});
 	  };
 	  $scope.board.push(row)
   }
-  $scope.print = function(x,y){
-  	alert("("+x+","+y+")")
+  $scope.click = function(x,y){
+  	if($scope.board[x][y].Player == "empty"){
+  		connection.send(JSON.stringify({"X":x,"Y":y,"Player":"empty"}))
+  	}
+  }
+});
+
+GoGoBan.controller('GoGoBanLobbyCtrl', function ($scope) {
+  $scope.players = []
+  $scope.name = ''
+  $scope.opponent = ''
+  var connection = new WebSocket('ws://'+window.location.host+'/ws/lobby', []);
+  connection.onmessage = function(e){
+  	obj = JSON.parse(e.data)
+  	if(obj.Source != undefined){
+  		$scope.opponent = obj.Source
+  		$('#modal').modal({})
+
+  	}else{
+	  	$scope.players = obj
+  	}
+	$scope.$apply()
+  }
+
+  $scope.sendName = function(name){
+  	$scope.name = name
+  	connection.send(name)
+  }
+  $scope.request = function(name){
+	$scope.opponent = name
+	$scope.$apply()
+	$('#request').modal({})
+  	connection.send(JSON.stringify({"Target":name,"Source":$scope.name}))
   }
 });
--- test.go
+++ test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/golang/glog"
+	"github.com/gorilla/websocket"
 )
 
 var dirFlag = flag.String("dir", "", "Directory with static content.")
@@ -20,7 +23,184 @@ func handler(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type Move struct {
+	X      int
+	Y      int
+	Player Player
+}
+
+type GameLink struct {
+	Source string
+	Target string
+}
+
+type Server struct {
+	Entrants  chan User
+	Exeunt    chan User
+	Players   []User
+	Broadcast chan bool
+	Request   chan GameLink
+	Accept    chan GameLink
+}
+
+type Player string
+
+const (
+	White Player = "white"
+	Black Player = "black"
+	None  Player = "empty"
+)
+
+func (p Player) Switch() Player {
+	switch p {
+	case White:
+		return Black
+	case Black:
+		return White
+	}
+	return None
+}
+
+type User struct {
+	Name       string
+	Connection *websocket.Conn
+}
+
+func boardhandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func gamehandler(w http.ResponseWriter, r *http.Request) {
+	player := Black
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for {
+		move := Move{}
+		err := conn.ReadJSON(&move)
+		if err != nil {
+			glog.Errorln(err)
+			continue
+		}
+		glog.V(1).Infoln(move)
+		move.Player = player
+		player = player.Switch()
+
+		err = conn.WriteJSON(move)
+		if err != nil {
+			glog.Errorln(err)
+			continue
+		}
+	}
+}
+
+func (s *Server) Lobby() {
+	for {
+		glog.V(1).Infoln("Checking lobby")
+		var u User
+		var gl GameLink
+		select {
+		case u = <-s.Entrants:
+			glog.V(1).Infoln("New player")
+			s.Players = append(s.Players, u)
+			broadcast(s.Players)
+		case u = <-s.Exeunt:
+			glog.V(1).Infoln("Player left")
+			s.Exit(u)
+		case gl = <-s.Request:
+			glog.V(1).Infoln("Requesting game:", gl)
+			target, err := findUser(s.Players, gl.Target)
+			if err != nil {
+				glog.Errorln(err)
+				continue
+			}
+			target.Connection.WriteJSON(gl)
+		case gl = <-s.Accept:
+			glog.V(1).Infoln("Accepting game:", gl)
+			target, err := findUser(s.Players, gl.Target)
+			if err != nil {
+				continue
+			}
+			src, err := findUser(s.Players, gl.Source)
+			if err != nil {
+				continue
+			}
+			s.Exit(target)
+			s.Exit(src)
+		}
+	}
+}
+
+func (s *Server) Exit(u User) {
+	if i, err := indexOf(s.Players, u); err != nil {
+		glog.Errorln(nil)
+	} else {
+		s.Players[i], s.Players = s.Players[len(s.Players)-1], s.Players[:len(s.Players)-1]
+		broadcast(s.Players)
+	}
+}
+
+func findUser(slice []User, name string) (User, error) {
+	for _, v := range slice {
+		if v.Name == name {
+			return v, nil
+		}
+	}
+	return User{}, fmt.Errorf("Element not found")
+}
+
+func indexOf(slice []User, elem User) (int, error) {
+	for i, v := range slice {
+		if v == elem {
+			return i, nil
+		}
+	}
+	return -1, fmt.Errorf("Element not found")
+}
+
+func broadcast(players []User) {
+	glog.V(1).Infoln("Broadcasting")
+	glog.V(1).Infoln(players)
+	names := []string{}
+	for _, p := range players {
+		names = append(names, p.Name)
+	}
+	for _, p := range players {
+		go p.Connection.WriteJSON(names)
+	}
+}
+
+func (s *Server) lobbyhandler(rw http.ResponseWriter, req *http.Request) {
+	conn, err := upgrader.Upgrade(rw, req, nil)
+	if err != nil {
+		glog.Errorln(err)
+		return
+	}
+	_, message, _ := conn.ReadMessage()
+	glog.Infoln("Player entered lobby:", string(message))
+	s.Entrants <- User{Connection: conn, Name: string(message)}
+	for {
+		glog.V(1).Infoln("Awaiting request")
+		gamelink := GameLink{}
+		err = conn.ReadJSON(&gamelink)
+		glog.V(1).Infoln("Request received:", gamelink)
+		if err != nil {
+			glog.Errorln(err)
+			s.Exeunt <- User{Connection: conn, Name: string(message)}
+			return
+		}
+		s.Request <- gamelink
+	}
+}
+
 func main() {
+
 	flag.Parse()
 	dir, err := filepath.Abs(*dirFlag)
 	if err != nil {
@@ -42,11 +222,24 @@ func main() {
 	glog.V(2).Infoln(css)
 	glog.V(2).Infoln(img)
 
+	server := Server{
+		Players:  []User{},
+		Exeunt:   make(chan User),
+		Entrants: make(chan User),
+		Request:  make(chan GameLink),
+		Accept:   make(chan GameLink),
+	}
+
 	http.Handle("/js/", http.StripPrefix("/js", http.FileServer(http.Dir(js))))
 	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir(css))))
 	http.Handle("/img/", http.StripPrefix("/img", http.FileServer(http.Dir(img))))
 
+	http.HandleFunc("/ws/lobby", server.lobbyhandler)
+	http.HandleFunc("/ws/game", gamehandler)
+	http.HandleFunc("/board", boardhandler)
 	http.HandleFunc("/", handler)
+
+	go server.Lobby()
 
 	http.ListenAndServe(":8080", nil)
 }
